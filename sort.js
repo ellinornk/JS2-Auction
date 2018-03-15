11166c305e2634c824fae2f4b40bbc2cc70db908
josefinesofie <32060043+josefinesofie@users.noreply.github.com>
2018-03-15 10:45:45 +0100

    sort changes

--- sort.js
+++ sort.js
@@ -1,7 +1,21 @@
-/*Sök
-Efter titel på auktion
+//sort by Utropspris
+//sort by SlutDatum
 
-Sortering - subklass
-Inläggets datum
-Utropspris
-*/
+
+sortingData();
+async function sortingData(){
+
+  let response = await fetchingData("http://nackowskis.azurewebsites.net/api/auktion/500");
+  for (let arr of response){
+    let sorted = arr.sort(response.Utropspris);
+    console.log(sorted);
+  }
+  return sorted;
+}
+
+
+async function fetchingData(url){
+  let promise = await fetch(url);
+  let data = await promise.json();
+  return data;
+}
